51a80780459bfa3ece8a7181e53087f6fbab054e
Adrienn Varn <adriennvarn@gmail.com>
2026-09-18 15:46:39 -0400

    search functionality added

--- src/__tests__/test_suites/SearchSort.test.jsx
+++ src/__tests__/test_suites/SearchSort.test.jsx
@@ -1,13 +1,11 @@
 import { render } from "@testing-library/react"
-import { beforeEach } from "vitest"
 import App from "../../components/App"
 import "@testing-library/jest-dom"
 
-beforeEach(() => {
-    global.setFetchResponse(global.baseTransactions)
-    const { findAllByTestId } = render(<App />)
-})
-
 describe("Our app will ", () => {
-    test.todo("update page onChange event")
+    test.todo("update page onChange event", () => {
+        // setup
+        global.setFetchResponse(global.baseTransactions)
+        const { findAllBy } = render(<App />)
+    })
 })
