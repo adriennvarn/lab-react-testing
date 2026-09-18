bd3ccecb9fdef2e0b4aa4f78b037677e21d82097
Adrienn Varn <adriennvarn@gmail.com>
2026-09-18 15:34:41 -0400

    finished addTransactions tests

--- src/__tests__/test_suites/AddTransactions.test.jsx
+++ src/__tests__/test_suites/AddTransactions.test.jsx
@@ -1,4 +1,4 @@
-import { render, act, fireEvent } from "@testing-library/react"
+import { render, fireEvent,  } from "@testing-library/react"
 import { userEvent } from "@testing-library/user-event"
 import App from "../../components/App"
 import "@testing-library/jest-dom"
@@ -6,22 +6,50 @@ import "@testing-library/jest-dom"
 describe("Our app will ", () => {
     test.only("add transactions to frontend", async () => {
         global.setFetchResponse(global.baseTransactions)
-        const { findByTestId, findByText } = render(<App />)
+        const { findAllByTestId, findAllByText } = render(<App />)
 
-        const addButton = await findByTestId("add-button")
-        const dateInput = await findByTestId("date-input")
-        const descriptionInput = await findByTestId("description-input")
-        const categoryInput = await findByTestId("category-input")
-        const amountInput = await findByTestId("amount-input")
+        const addButton = (await findAllByTestId("add-button"))[0]
+        const dateInput = (await findAllByTestId("date-input"))[0]
+        const descriptionInput = (await findAllByTestId("description-input"))[0]
+        const categoryInput = (await findAllByTestId("category-input"))[0]
+        const amountInput = (await findAllByTestId("amount-input"))[0]
 
         fireEvent.change(dateInput, { target: { value: "2026-09-18" } })
         await userEvent.type(descriptionInput, "Test transaction")
         await userEvent.type(categoryInput, "Test category")
         await userEvent.type(amountInput, "12.00")
 
-        await userEvent.click(addButton)
+        const mockPostData =  { id: 999, date: "2026-09-18", description: "Test transaction", category: "Test category", amount: "12" }
+        global.setFetchResponse(mockPostData)
 
-        const newTransaction = await findByText("Test transaction")
+        fireEvent.click(addButton)
+
+        const newTransaction = (await findAllByText("Test transaction"))[0]
         expect(newTransaction).toBeInTheDocument()
+    })
+
+    test("correctly POST new transactions", async () => {
+        global.setFetchResponse(global.baseTransactions)
+        const { findAllByTestId } = render(<App />)
+
+        const addButton = (await findAllByTestId("add-button"))[0]
+        const dateInput = (await findAllByTestId("date-input"))[0]
+        const descriptionInput = (await findAllByTestId("description-input"))[0]
+        const categoryInput = (await findAllByTestId("category-input"))[0]
+        const amountInput = (await findAllByTestId("amount-input"))[0]
+
+        fireEvent.change(dateInput, { target: { value: "2026-09-18" } })
+        await userEvent.type(descriptionInput, "Test transaction")
+        await userEvent.type(categoryInput, "Test category")
+        await userEvent.type(amountInput, "12.00")
+
+        const targetResponse = { date: "2026-09-18", description: "Test transaction", category: "Test category", amount: "12" }
+        global.setFetchResponse(targetResponse)
+        fireEvent.click(addButton)
+        expect(fetch).toHaveBeenCalledWith("http://localhost:6001/transactions", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(targetResponse)
+        })
     })
 })
