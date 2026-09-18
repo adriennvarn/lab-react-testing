5768b0cd5d0b3518cdf52eda5a4c728c831b7644
Adrienn Varn <adriennvarn@gmail.com>
2026-09-18 15:39:37 -0400

    commented

--- src/__tests__/test_suites/AddTransactions.test.jsx
+++ src/__tests__/test_suites/AddTransactions.test.jsx
@@ -3,49 +3,66 @@ import { userEvent } from "@testing-library/user-event"
 import App from "../../components/App"
 import "@testing-library/jest-dom"
 
+// I experienced SIGNIFICANT problems with items being duplicated. This is why
+// all of the inputs use findAll and index 0. 
+
 describe("Our app will ", () => {
-    test.only("add transactions to frontend", async () => {
+    test("add transactions to frontend", async () => {
+        // setup
         global.setFetchResponse(global.baseTransactions)
         const { findAllByTestId, findAllByText } = render(<App />)
 
+        // find inputs
         const addButton = (await findAllByTestId("add-button"))[0]
         const dateInput = (await findAllByTestId("date-input"))[0]
         const descriptionInput = (await findAllByTestId("description-input"))[0]
         const categoryInput = (await findAllByTestId("category-input"))[0]
         const amountInput = (await findAllByTestId("amount-input"))[0]
 
+        // update inputs with mock values
         fireEvent.change(dateInput, { target: { value: "2026-09-18" } })
         await userEvent.type(descriptionInput, "Test transaction")
         await userEvent.type(categoryInput, "Test category")
         await userEvent.type(amountInput, "12.00")
 
+        // set mock data to satisfy POST
         const mockPostData =  { id: 999, date: "2026-09-18", description: "Test transaction", category: "Test category", amount: "12" }
         global.setFetchResponse(mockPostData)
 
+        // FIRE!
         fireEvent.click(addButton)
 
+        // verify that the description found it's way into the document
         const newTransaction = (await findAllByText("Test transaction"))[0]
         expect(newTransaction).toBeInTheDocument()
     })
 
     test("correctly POST new transactions", async () => {
+        // setup
         global.setFetchResponse(global.baseTransactions)
         const { findAllByTestId } = render(<App />)
 
+        // find inputs
         const addButton = (await findAllByTestId("add-button"))[0]
         const dateInput = (await findAllByTestId("date-input"))[0]
         const descriptionInput = (await findAllByTestId("description-input"))[0]
         const categoryInput = (await findAllByTestId("category-input"))[0]
         const amountInput = (await findAllByTestId("amount-input"))[0]
 
+        // update inputs with mock values
         fireEvent.change(dateInput, { target: { value: "2026-09-18" } })
         await userEvent.type(descriptionInput, "Test transaction")
         await userEvent.type(categoryInput, "Test category")
         await userEvent.type(amountInput, "12.00")
 
+        // set intended response
         const targetResponse = { date: "2026-09-18", description: "Test transaction", category: "Test category", amount: "12" }
         global.setFetchResponse(targetResponse)
+
+        // FIRE
         fireEvent.click(addButton)
+
+        // verify fetch was called correctly
         expect(fetch).toHaveBeenCalledWith("http://localhost:6001/transactions", {
             method: "POST",
             headers: {"Content-Type": "application/json"},
--- src/__tests__/test_suites/DisplayTransactions.test.jsx
+++ src/__tests__/test_suites/DisplayTransactions.test.jsx
@@ -4,11 +4,15 @@ import "@testing-library/jest-dom"
 
 describe("Our app will ", () => {
     test("display all users on startup", async () => {
+        // setup
         global.setFetchResponse(global.baseTransactions)
         const { findAllByTestId } = render(<App />)
+
+        // count transaction items to ensure all are displayed
         const transactionItems = await findAllByTestId("transaction-item")
         expect(transactionItems).toHaveLength(global.baseTransactions.length)
 
+        // match descriptions
         const transactionDescriptions = transactionItems.map((item) => item.querySelectorAll("td")[1].textContent)
         const baseDescriptions = global.baseTransactions.map((item) => item.description)
         expect(transactionDescriptions).toEqual(baseDescriptions)
